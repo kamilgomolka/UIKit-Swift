d0a528bc10e62b4ac658a40c12b1d227277aaf81
Kamil Gomółka <kamil.gomolka@gmail.com>
2022-03-30 15:53:27 +0200

    Buttons [3]

--- UIKit-Swift/Source/ViewControllers/Main Menu ViewControllers/Button/ViewControllers/OtherButtonsViewController.swift
+++ UIKit-Swift/Source/ViewControllers/Main Menu ViewControllers/Button/ViewControllers/OtherButtonsViewController.swift
@@ -33,6 +33,85 @@ class OtherButtonsViewController: UIViewController {
         return stackView
     }()
     
+    var buttonDetailDisclosure: UIButton = {
+        let button = UIButton(type: .detailDisclosure)
+        button.configuration = UIButton.Configuration.gray()
+        button.configuration?.title = "type = .detailDisclosure"
+        return button
+    }()
+    
+    var buttonContactAdd: UIButton = {
+        let button = UIButton(type: .contactAdd)
+        button.configuration = UIButton.Configuration.gray()
+        button.configuration?.title = "type = .contactAdd"
+        return button
+    }()
+
+    var buttonClose: UIButton = {
+        let button = UIButton(type: .close)
+        button.configuration = UIButton.Configuration.gray()
+        button.configuration?.title = "type = .close"
+        return button
+    }()
+
+    var buttonPrimary: UIButton = {
+        var config = UIButton.Configuration.gray()
+        config.title = "role = .primary"
+        
+        let button = UIButton(configuration: config)
+        button.role = .primary
+        return button
+    }()
+
+    var buttonActivityIndicator: UIButton = {
+        var config = UIButton.Configuration.gray()
+        config.title = "showsActivityIndicator = true"
+        config.showsActivityIndicator = true
+        return UIButton(configuration: config)
+    }()
+    
+    var buttonSubtitle: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.subtitle = "Just subtitle"
+        return UIButton(configuration: config)
+    }()
+    
+    var buttonTitleAndSubtitle: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "Title"
+        config.subtitle = "Subtitle"
+        return UIButton(configuration: config)
+    }()
+    
+    var buttonImage: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.image = UIImage(systemName: "square.and.arrow.up")
+        return UIButton(configuration: config)
+    }()
+    
+    var buttonImageAndText: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.image = UIImage(systemName: "square.and.arrow.up")
+        config.title = "Image and text"
+        return UIButton(configuration: config)
+    }()
+    
+    var buttonImageAndTextAndSubtitle: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.image = UIImage(systemName: "square.and.arrow.up")
+        config.title = "Title"
+        config.subtitle = "Subtitle"
+        return UIButton(configuration: config)
+    }()
+    
+    var buttonDisabled: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "Disabled"
+        let button = UIButton(configuration: config)
+        button.isEnabled = false
+        return button
+    }()
+    
     let clicksCounter = ButtonClicksCounter()
     
     // MARK: - Livecycle
@@ -54,10 +133,17 @@ class OtherButtonsViewController: UIViewController {
         contentView.addSubview(stackView)
         
         let buttons = [
-            newButton(title: "Mini",    size: .mini),
-            newButton(title: "Small",   size: .small),
-            newButton(title: "Medium",  size: .medium),
-            newButton(title: "Large",   size: .large)
+            buttonDetailDisclosure,
+            buttonContactAdd,
+            buttonClose,
+            buttonPrimary,
+            buttonActivityIndicator,
+            buttonSubtitle,
+            buttonTitleAndSubtitle,
+            buttonImage,
+            buttonImageAndText,
+            buttonImageAndTextAndSubtitle,
+            buttonDisabled
         ]
         
         clicksCounter.setup(buttons: buttons)
@@ -65,16 +151,6 @@ class OtherButtonsViewController: UIViewController {
         for button in buttons {
             stackView.addArrangedSubview(button)
         }
-    }
-    
-    func newButton(title: String, size: UIButton.Configuration.Size) -> UIButton {
-        var config = UIButton.Configuration.filled()
-        config.title = title
-        config.buttonSize = size
-        
-        let button = UIButton(configuration: config)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
     }
     
     func createConstraints() {
